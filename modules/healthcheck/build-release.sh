c9ea8c9a834c6d43d277d528de4f032684b111c6
Olaf Kock <olaf.kock+github@liferay.com>
2025-04-04 11:02:41 +0200

    update MP builds

--- modules/healthcheck/build-release.sh
+++ modules/healthcheck/build-release.sh
@@ -2,7 +2,6 @@
 
 function revert_it_all {
     echo reverting changes
-    mv */build/libs/*.jar ce/
     cp ../../gradle.properties.bak ../../gradle.properties
     mv healthcheck-api/build.gradle.bak healthcheck-api/build.gradle
     mv healthcheck-bestpractice/build.gradle.bak healthcheck-bestpractice/build.gradle
@@ -16,16 +15,6 @@ function revert_it_all {
     mv healthcheck-web/bnd.bnd.bak healthcheck-web/bnd.bnd
 }
 
-function dont_build_it_all {
-    echo $2
-    echo '$2'
-    echo "$2"
-    echo Bla $2 Bla
-    echo 'Bla $2 Bla'
-    echo "Bla $2 Bla"
-}
-
-
 function build_it_all {
     echo Building $2
     sed -i.bak "s/release.dxp.api/$1/" */build.gradle
@@ -33,8 +22,13 @@ function build_it_all {
     sed -i.bak "s/liferay.workspace.product/\#liferay.workspace.product/" ../../gradle.properties
     printf "\nliferay.workspace.product=$3" >> ../../gradle.properties
     ../../gradlew clean jar
-    mkdir $2
-    mv */build/libs/*.jar $2/
+    if [ -f healthcheck-api/build/libs/*.jar ]; then
+        mkdir $2
+        mv */build/libs/*.jar $2/
+    else
+        echo "Build Problem? Can't find output"
+        sleep 3
+    fi
     echo Reverting files after $2
     revert_it_all
     sleep 2
@@ -44,11 +38,11 @@ function build_it_all {
 
 build_it_all release.dxp.api DXP-2025-Q1 dxp-2025.q1.1-lts
 build_it_all release.dxp.api DXP-2024-Q4 dxp-2024.q4.1
-build_it_all release.dxp.api DXP-2024-Q3 dxp-2024.q3.1
-build_it_all release.dxp.api DXP-2024-Q2 dxp-2024.q2.1
-build_it_all release.dxp.api DXP-2024-Q1 dxp-2024.q1.1
-build_it_all release.dxp.api DXP-2023-Q4 dxp-2023.q4.1
+# build_it_all release.dxp.api DXP-2024-Q3 dxp-2024.q3.1
+# build_it_all release.dxp.api DXP-2024-Q2 dxp-2024.q2.1
+# build_it_all release.dxp.api DXP-2024-Q1 dxp-2024.q1.1
+# build_it_all release.dxp.api DXP-2023-Q4 dxp-2023.q4.1
 build_it_all release.portal.api CE-GA132 portal-7.4-ga132
 build_it_all release.portal.api CE-GA129 portal-7.4-ga129
-build_it_all release.portal.api CE-GA125 portal-7.4-ga125
-build_it_all release.portal.api CE-GA120 portal-7.4-ga120
+# build_it_all release.portal.api CE-GA125 portal-7.4-ga125
+# build_it_all release.portal.api CE-GA120 portal-7.4-ga120
