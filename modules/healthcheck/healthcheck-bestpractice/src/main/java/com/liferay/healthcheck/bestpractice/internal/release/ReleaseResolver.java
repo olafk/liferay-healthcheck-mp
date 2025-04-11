f78bb95e9c63dfcfe65377ae9955aead2a5d24e3
Olaf Kock <olaf.kock+github@liferay.com>
2025-04-11 17:47:27 +0200

    first take on release age and newer available releases

---
+++ modules/healthcheck/healthcheck-bestpractice/src/main/java/com/liferay/healthcheck/bestpractice/internal/release/ReleaseResolver.java
@@ -0,0 +1,54 @@
+package com.liferay.healthcheck.bestpractice.internal.release;
+
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.HttpUtil;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.LinkedList;
+
+public class ReleaseResolver {
+	public static Collection<ReleaseInformation> retrieveReleases() throws IOException {
+		if(JSONFactoryUtil.getJSONFactory() == null) return null; // not yet initialized
+
+		LinkedList<ReleaseInformation> result = new LinkedList<ReleaseInformation>();
+		try {
+			String json = null;
+			try {
+				// Note: getVersionDisplayName was introduced - according to LPS-144747 - in DXP 7.4 Update 3 / CE 7.4 GA7
+				String location = "https://releases-cdn.liferay.com/releases.json";
+				json = HttpUtil.URLtoString(location);
+				log.info("read " + json.length() + " characters config from " + location);
+			} catch (IOException e) {
+				log.warn("Couldn't resolve up-to-date release content due to " + e.getMessage() + " " + e.getClass().getName());
+			} catch (NullPointerException e) {
+				return null; // not yet initialized
+			}
+			ReleaseInformation[] releaseInfos = JSONFactoryUtil.looseDeserialize(json, ReleaseInformation[].class);
+			for (ReleaseInformation releaseInfo : releaseInfos) {
+				result.add(releaseInfo);
+			}
+		} catch (Exception e) {
+			log.error(e);
+		}
+//		Collections.sort(result, new ReleaseComparator());
+//		for (ReleaseInformation ri : result) {
+//			System.out.println(ri.getReleaseDate());
+//		}
+		return result;
+	}
+
+	public static final Log log = LogFactoryUtil.getLog(ReleaseResolver.class);
+
+	public static class ReleaseComparator implements Comparator<ReleaseInformation>{
+
+		@Override
+		public int compare(ReleaseInformation arg0, ReleaseInformation arg1) {
+			return arg0.getReleaseDate().compareTo(arg1.getReleaseDate());
+		}
+		
+	}
+}
